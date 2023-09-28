f6d1c9567d17f913ab3612dd0a2a560bd01c3f73
ShahidBahadur <shahidbahadur1998@gmail.com>
2023-09-28 16:50:57 +0500

    fixes done

--- scripts/slidebar.js
+++ scripts/slidebar.js
@@ -1,36 +1,22 @@
 
-var screenWidth;
-function getScreenSizeOnResize() {
-    const sidebar = document.getElementById("sidebar");
-    function updateScreenSize() {
-        screenWidth = window.screen.width;
-        if (screenWidth > 992) {
-            sidebar?.classList.remove("top-0");
-            sidebar?.classList.add("-top-[140vh]");
-        }
-    }
-    updateScreenSize();
-    window.addEventListener("resize", updateScreenSize);
-}
-document.addEventListener("DOMContentLoaded", getScreenSizeOnResize);
-
+var  sidebar = document.getElementById("sidebar");
 
 function closeSidebar() {
+    const sidebar = document.getElementById("sidebar");
     sidebar?.classList.remove("top-0");
-    sidebar?.classList.add("-top-[140vh]");
+    sidebar?.classList.add("-translate-y-[150vh]");
 }
 
 function openSidebar() {
-    sidebar?.classList.remove("-top-[140vh]");
+    const sidebar = document.getElementById("sidebar");
+    sidebar?.classList.remove("-translate-y-[150vh]");
     sidebar?.classList.add("top-0");
 }
 
-//  JavaScript to toggle the sidebar
 const hamburger = document.getElementById("hamburger");
-const sidebar = document.getElementById("sidebar");
 const close = document.getElementById("close");
-const links = document.querySelectorAll("#sidebar a");
 
-document.addEventListener("DOMContentLoaded", closeSidebar);
 hamburger?.addEventListener("click", openSidebar);
 close?.addEventListener("click", closeSidebar);
+
+document.addEventListener("DOMContentLoaded", closeSidebar);
